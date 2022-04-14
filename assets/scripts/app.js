fc251a9cc8f80aeeab4abbf5f2225addb82fe7f5
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2022-04-14 17:55:37 -0400

    installed lodash and axios via cdn

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,8 @@
 // alert('js connected');
+const customers = ['Matt', 'Sandra', 'Liam', 'Connor'];
+
+const activeCustomers = ['Matt', 'Sandra'];
+
+const inactiveCustomers = _.difference(customers, activeCustomers);
+
+console.log(inactiveCustomers);
